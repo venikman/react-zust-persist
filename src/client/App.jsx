2442902f95b6152b14e364216c4af374668fefc7
venikman <1397672-venikman@users.noreply.replit.com>
2025-01-23 10:57:55 +0000

    User checkpoint: Refactor application to use client-side rendering.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,9 +1,9 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from '../pages/Home';
-import Redirect from '../pages/Redirect';
-import Preview from '../pages/Preview';
+import Home from './pages/Home';
+import Redirect from './pages/Redirect';
+import Preview from './pages/Preview';
 
 export default function App() {
   return (
